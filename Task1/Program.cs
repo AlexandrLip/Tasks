e65ce77c1e6cff6af02f4a2292c82bf551be7bb0
Alex <lipatnikov@yandex.ru>
2022-04-11 23:10:16 +0500

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine($"введите первое число: ");
+int number1 = Console.ReadLine();
+Console.WriteLine($"введите второе число: ");
+
+int number2 = Console.ReadLine();
+
+if(number2 == number1);
+{
+    Console.WriteLine($"a = {number1}; b = {number2} -> да");
+}
+else
+{
+    Console.WriteLine($"a = {number1}; b = {number2} -> нет");
+}
