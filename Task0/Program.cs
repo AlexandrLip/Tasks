db67d99ba0554095def098da66a96e50732f18b7
Alex <lipatnikov@yandex.ru>
2022-04-11 23:10:06 +0500

    Add Task0

---
+++ Task0/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using static System.Console;
+
+WriteLine("Input number");
+int number = int.Parse(ReadLine());
+int sqr = number*number;
+WriteLine(sqr);
+WriteLine($"Result = {sqr}");
+
