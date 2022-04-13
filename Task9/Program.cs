d5166e9a9ffd0c5fa5c501f2c97c93b2d1e63999
Alex <lipatnikov@yandex.ru>
2022-04-13 22:23:00 +0500

    Add указание минимального числа

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -4,9 +4,9 @@ Console.WriteLine($"введите второе число: ");
 int number2 = int.Parse(Console.ReadLine());
 if (number2 < number1)
 {
-    Console.WriteLine($"a = {number1}; b = {number2} -> max = {number1}");
+    Console.WriteLine($"a = {number1}; b = {number2} -> max = {number1}, min = {number2}");
 }
     else
 {
-    Console.WriteLine($"a = {number1}; b = {number2} -> max = {number2}"); 
+    Console.WriteLine($"a = {number1}; b = {number2} -> max = {number2}, min = {number1}"); 
 }
