4bc8fbe15637048e66a6ba2733be4804e1e34e2e
Alex <lipatnikov@yandex.ru>
2022-04-13 22:10:15 +0500

    Add Task9

---
+++ Task9/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine($"введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine($"введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number2 < number1)
+{
+    Console.WriteLine($"a = {number1}; b = {number2} -> max = {number1}");
+}
+    else
+{
+    Console.WriteLine($"a = {number1}; b = {number2} -> max = {number2}"); 
+}
