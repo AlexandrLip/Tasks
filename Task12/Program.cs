e7e85bf57c79f9b1e9b38a897083a51aeabb1a44
Alex <lipatnikov@yandex.ru>
2022-04-13 23:35:49 +0500

    Add Task12

---
+++ Task12/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+Console.Write("Введите число : ");
+int N = int.Parse(Console.ReadLine());
+int B = 1;
+
+while (B<=N)
+{
+    if (B % 2 == 0)
+        Console.WriteLine($"{B} ");
+        B = B + 1;
+}
+ 
