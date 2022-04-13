1a8767a76a10d3cc4d46e4aa776f0aa37682441a
Alex <lipatnikov@yandex.ru>
2022-04-13 23:06:53 +0500

    Add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,12 @@
+﻿ 
+Console.Write("Введите число : ");
+int number1 = int.Parse(Console.ReadLine());
+ 
+if (number1 % 2 == 0)
+ {
+ Console.Write("Введенное число является четным");
+}
+else
+{
+Console.Write("Введенное число нечетное");
+}
