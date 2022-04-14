7ddf8c0b4e4a317cc7c5d3cea6605fc73d8db745
Alex <lipatnikov@yandex.ru>
2022-04-14 23:37:56 +0500

    Add Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,28 @@
+﻿
+/*Напишите программу, которая принимает на вход число и
+проверяет, кратно ли оно одновременно 7 и 23.
+
+14  ->  нет
+46  ->  нет
+161 ->  да*/
+
+using System;
+
+Console.WriteLine("Ведите первое число");
+int numA = int.Parse(Console.ReadLine());
+
+int numB = 23;
+int numC = 7;
+
+
+/*if(numA%numB%numC ==0)
+{
+    Console.WriteLine($"Число {numB} кратно числу {numC}");
+}
+else
+{
+    Console.WriteLine($"Число {numC} не кратно числу {numA}");
+}*/
+
+string answer=((numA%numC==0)&&(numA%numB==0))?"кратно":"не кратно";
+Console.Write(answer);
