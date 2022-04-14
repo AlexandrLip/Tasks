51940d1ccde47ec1588e9753a837d789126a8e84
Alex <lipatnikov@yandex.ru>
2022-04-14 22:21:33 +0500

    Add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.Clear();
+
+int num = new Random(). Next(10,100);
+string numS=num.ToString();
+int a1=Convert.ToInt32(numS[0].ToString());
+int a2=Convert.ToInt32(numS[1].ToString());
+//int a1=num/10;
+//int a2=num%10;
+
+if(a1>a2)
+    Console.WriteLine($"{num}->{a1}");
+else   
+    Console.WriteLine($"{num}->{a2}");
