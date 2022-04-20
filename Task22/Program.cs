b084209c3116593fb4ca069da6ffc7f2241390b3
Alex <lipatnikov@yandex.ru>
2022-04-20 22:45:51 +0500

    Add Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,37 @@
+﻿//введите номер четверти
+
+
+Console.Clear();
+
+Console.WriteLine("Ведите номер четверти");
+int num = int.Parse(Console.ReadLine());
+
+switch(num) 
+{
+    case 1:
+    {
+        Console.WriteLine("x>0 y>0");
+    break;
+    }
+    case 2:
+    {
+        Console.WriteLine("x<0 y>0");
+    break;
+    }
+    case 3:
+    {
+        Console.WriteLine("x<0 y<0");
+    break;
+    }
+    case 24:
+    {
+        Console.WriteLine("x>0 y<0");
+    break;
+    }
+
+    default:
+    {
+        Console.WriteLine("введено не правильное число!");
+        break;
+    }
+}
