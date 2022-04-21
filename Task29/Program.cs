923149d9b7e7ca0629b3d3dfac1f6aabf3262f50
Alex <lipatnikov@yandex.ru>
2022-04-21 23:31:26 +0500

    Add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает
+на вход число N и выдаёт произведение чисел от 1 до N.
+
+4  -> 24
+5  -> 120
+*/
+
+using System;
+Console.Clear();
+Console.Write("Введите N: ");
+
+Console.WriteLine(Mult(int.Parse(Console.ReadLine()!)));
+
+
+int Mult(int number)
+{
+    int sub=1;
+    for(int i=1; i<=number; i++)
+    {
+        sub = sub*i;
+    }
+    return sub;
+}
