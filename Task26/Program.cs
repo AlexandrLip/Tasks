9c75aaf9d2624a4b0404968c2e5d8b6d3ae26524
Alex <lipatnikov@yandex.ru>
2022-04-21 22:15:39 +0500

    Add Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Напишите программу, которая принимает на вход число (А)
+и выдаёт сумму чисел от 1 до А.
+7 -> 28
+
+4 -> 10
+
+8 -> 36
+*/
+
+using System;
+Console.Clear();
+Console.Write("Введите А: ");
+int A=int.Parse(Console.ReadLine());
+Console.WriteLine(Summa(A));
+
+
+int Summa(int number)
+{
+    int sum=0;
+    for(int i=0; i<=number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
