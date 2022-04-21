36e13115103c524f9fa4c4506c22662e63d980a3
Alex <lipatnikov@yandex.ru>
2022-04-21 20:49:45 +0500

    Add Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное
+число и проверяет, является ли оно палиндромом.
+
+*/
+
+Console.Clear();
+
+Console.WriteLine("Ведите пятизначное число");
+int num = int.Parse(Console.ReadLine());
+
+int a = num / 10000;
+int b = num % 10000 / 1000;
+int c = num % 1000 / 100;
+int d = num % 100 / 10;
+int e = num % 10;
+
+if (a == e && b == d)
+    {
+    Console.WriteLine("Введенное число палиндром");
+    }
+    else
+    {
+    Console.WriteLine("Введенное число не является палиндромом");
+    }
+    
