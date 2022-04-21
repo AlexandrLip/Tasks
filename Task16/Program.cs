f16adee31daab11368d93a98617b2d98bb62212f
Alex <lipatnikov@yandex.ru>
2022-04-21 20:50:21 +0500

    Add Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.WriteLine("Ведите первое число");
+int numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ведите второе число");
+int numB = int.Parse(Console.ReadLine());
+
+//int numC = numA%numB==0;
+
+if(numA%numB ==0)
+{
+    Console.WriteLine($"Число {numA} кратно числу {numB}");
+}
+else
+{
+    Console.WriteLine($"Число {numA} не кратно числу {numB}");
+}
