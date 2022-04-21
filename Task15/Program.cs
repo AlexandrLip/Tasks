409a582cdfe3dae0af80542117e11e5f6aca2827
Alex <lipatnikov@yandex.ru>
2022-04-21 20:50:12 +0500

    Add Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,10 @@
+﻿
+int num = new Random().Next(100,1000);
+string numS = num.ToString();
+
+int first = Convert.ToInt32(numS[0].ToString());
+int Third = Convert.ToInt32(numS[2].ToString());
+
+WriteLine(num);
+Write(first);
+Write(third);
