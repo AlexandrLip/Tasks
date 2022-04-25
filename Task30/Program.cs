02de0f6e5617e69c0b0ae1532cee8481526b70b7
Alex <lipatnikov@yandex.ru>
2022-04-25 21:10:30 +0500

    Add Task32

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -5,7 +5,7 @@
 [1,0,1,1,0,1,0,0]
 */
 
-using System;
+/*using System;
 
 Console.Clear();
 int[] r = new int[8];
@@ -15,3 +15,16 @@ for (int i = 0; i < 8; i++)
     r[i] = new Random().Next(0,2);
 }
 Console.WriteLine(String.Join(" ", r));
+*/
+int k = int.Parse(Console.ReadLine());
+Console.WriteLine(string.Join(" ",Array(k)));
+
+int [] Array(int l)
+{
+    int [] arr = new int [l];
+    for(int i = 0; i < l; i ++)
+    {
+        arr[i] = new Random().Next(2);
+    }
+    return arr;
+}
---
+++ Task32/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая принимает на вход
+координаты двух точек и находит расстояние между
+ними в 3D пространстве.
+
+A (3,6,8); B (2,1,-7), -> 15.84
+
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+using System;
+Console.Clear();
+
+Console.Write("Введите координаты точки А в формате (x, y, z): ");
+string[] r = Console.ReadLine().Split(",");
+Console.Write("Введите координаты точки В в формате (x, y, z): ");
+string[] t = Console.ReadLine().Split(",");
+int[] a = new int[3];
+int[] b = new int[3];
+
+int result = 0;
+for (int i = 0; i < 3; i++)
+{
+    a[i] = int.Parse(r[i]);
+    b[i] = int.Parse(t[i]);
+}
+
+for (int i = 0; i < a.Length; i++)
+{
+    result += (b[i] - a[i]) * (b[i] - a[i]);
+}
+Console.WriteLine($"Расстояние  = {Math.Sqrt(result):f2}");
+
