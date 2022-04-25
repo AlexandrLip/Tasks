57bb3f40972f73c24bd2486c92592d947b4f5be6
Alex <lipatnikov@yandex.ru>
2022-04-26 00:10:41 +0500

    Add Task40

---
+++ Task40/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+using System;
+using System.Linq;
+
+Console.Clear();
+int[] k=new int[20].Select(x=>new Random().Next(0,1000)).Where(x=>x>=9&&x<=100).ToArray();
+Console.WriteLine(String.Join(" ",k));
