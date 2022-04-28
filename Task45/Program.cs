6ceacd715eba41311ea5fd9fe92b87816ba232b4
Alex <lipatnikov@yandex.ru>
2022-04-28 23:13:26 +0500

    Add Task45

---
+++ Task45/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход
+// три числа и проверяет, может ли существовать
+// треугольник с сторонами такой длины.
+
+// ❗ **Теорема о неравенстве треугольника:**
+// каждая сторона треугольника меньше суммы двух других сторон.
+
+
+// int A = 3;
+// int B = 10;
+// int C = 3;
+
+
+
+// if ((A+B) > C  && (B+C)>A && (C+A) >B)
+// {
+//     WriteLine("является треугольником");
+// }
+// else
+// {
+//     WriteLine("не является треугольником");
+// }
+
+
+using System;
+using System.Linq;
+Console.Clear();
+
+Console.WriteLine("Введите стороны треугольника через пробел");
+string[] trengle = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int num1 = int.Parse(trengle[0]);
+int num2 = int.Parse(trengle[1]);
+int num3 = int.Parse(trengle[2]);
+
+Console.WriteLine((num1 < num2 + num3) && (num2 < num1 + num3) && (num3 < num2 + num1)? "Такой треугольник может существовать.": "Такого треугольника быть не может");
