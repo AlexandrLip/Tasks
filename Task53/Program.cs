0444efb4ea304050c5fd9506f55f65f6a84b9b86
Alex <lipatnikov@yandex.ru>
2022-05-04 10:43:02 +0500

    Add Task53

---
+++ Task53/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+
+Пользователь вводит с клавиатуры M чисел.
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3
+*/
+
+using static System.Console;
+Console.Clear();
+
+Console.Write("Введите числа через запятую и пробел: ");
+int[] Arr = Array.ConvertAll(Console.ReadLine().Split(", "), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < Arr.Length; i++)
+{
+    if (Arr[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов больше 0: {count}");
