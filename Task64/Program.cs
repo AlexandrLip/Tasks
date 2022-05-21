e73a87a74926bf43b1c33bfa913692f4d0ebd4ee
Alex <lipatnikov@yandex.ru>
2022-05-21 10:16:38 +0500

    Add Task64

--- Task64/Program.cs
+++ Task64/Program.cs
@@ -9,12 +9,15 @@ N = 6 -> "1, 2, 3, 4, 5, 6"
 using System;
 using static System.Console;
 Clear();
-int N=5;
+// int N=5;
+Write("Введите число: ");
+int N = int.Parse(ReadLine()!);
 
 string result = GetNumbers(1, N);
-WriteLine(result);
+WriteLine($"Натуральные числа в промежутке от 1 до введенного вами: {result}");
 static string GetNumbers(int start, int end)
 {
     if(start==end) return start.ToString();
     return(start+" "+GetNumbers(start+1,end));
 }
+// Console.WriteLine
