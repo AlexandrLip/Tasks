a70719a0bfcd43489bc10cc9074b9d6a693d8e88
Alex <lipatnikov@yandex.ru>
2022-05-25 23:21:20 +0500

    Add Task76

---
+++ Task76/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 66: Задайте значения M и N.
+Напишите программу, которая найдёт сумму натуральных
+элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введи через пробел два числа для определения суммы натуральных чисел между ними:");
+int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+
+int m = numbers[0];
+int n = numbers[1];
+WriteLine(Summ(m, n));
+
+int Summ(int start, int end)
+{
+    if (start == end) return end;
+    return (start + Summ(start + 1, end));
+}
